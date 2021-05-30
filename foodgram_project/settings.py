dd6b9a17c5d18ed71554fd80b71a812da1fe2732
Danila <dankol@mail.ru>
2021-05-30 19:23:43 +0300

    second

--- foodgram_project/settings.py
+++ foodgram_project/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', '0') == '1'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    '84.252.135.25', 'localhost',
+    'foodgramm.tk', 'www.foodgramm.tk'
+]
 
 # Application definition
 
